30a29693c1c32915d2098a5fa3d4ab51ee1d46a0
PatoGM <patriciogm@live.com>
2022-07-24 15:18:37 -0500

    testing stream and reader code

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ import { Http3Server } from '@fails-components/webtransport'
 console.log("Success!")
 console.log("Success 2!")
 console.log("Success latest!")
+
+const server = new Http3Server
+(
+    {
+        port: 44331,
+        host: "127.0.0.1",
+    }
+)
+
+const stream = await server.sessionStream('/')
+
+const reader = stream.getReader()
+
+console.log(reader)
