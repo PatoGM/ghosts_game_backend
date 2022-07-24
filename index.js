e8fd58215bb0e4d4dbb62156db7ba2c085329104
PatoGM <patriciogm@live.com>
2022-07-24 15:23:17 -0500

    testing stream and reader code, added secret now, then cert based on source code

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import { Http3Server } from '@fails-components/webtransport'
+import { generateWebTransportCertificate } from './certificate.js'
+
+const attrs = [
+    { shortName: 'C', value: 'USA' },
+    { shortName: 'ST', value: 'Texas' },
+    { shortName: 'L', value: 'Houston' },
+    { shortName: 'O', value: 'WebTransport Test Server: Chase + Pato' },
+    { shortName: 'CN', value: '127.0.0.1' }
+]
+
+const certificate = await generateWebTransportCertificate(attrs, {
+    days: 13
+})
 
 console.log("Success!")
 console.log("Success 2!")
@@ -9,7 +22,9 @@ const server = new Http3Server
         {
             port: 44331,
             host: "127.0.0.1",
-            secret: "mysecret"
+            secret: "mysecret",
+            cert: certificate.cert,
+            privKey: certificate.private
         }
     )
 
