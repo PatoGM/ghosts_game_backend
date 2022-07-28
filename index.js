44b10e50c33696b6f8f9ae2067fd537404ba6d5a
PatoGM <patriciogm@live.com>
2022-07-28 12:22:26 -0500

    testing file read

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const attrs = [
 // console.log(cert_text)
 
 // const path = '../Chase + Pato - Ghosts - Latest/cert.txt'
-const path = './'
+const path = './node'
 
 // console.log(__dirname);
 // console.log(path.dirname(__filename));
